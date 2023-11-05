0c0825073c036a49734cf057078e91084d7a2410
eugene <umid09tuxtayev@gmail.com>
2023-11-05 15:57:05 +0500

    feat: source btn

---
+++ scenes/source.ts
@@ -0,0 +1,25 @@
+import {Scene} from "grammy-scenes";
+import {BotContext} from "#types/context";
+import HLanguage from "#helper/language";
+import {HReplace} from "#helper/replacer";
+
+const scene = new Scene<BotContext>('Source');
+
+scene.step(async (ctx) => {
+    let sourceReplyMessage = HLanguage('sourceMessage')
+    const regionTimeLink = 'https://islom.uz/vaqtlar/' + ctx.user.regionId + '/' + (new Date().getMonth() + 1);
+
+    sourceReplyMessage = HReplace(
+        sourceReplyMessage,
+        ['$siteLink', '$sourceLink'],
+        [regionTimeLink, 'https://www.ziyouz.com/books/islomiy/hadis/Imom%20Navaviy.%20Riyozus%20solihiyn.pdf']
+    )
+
+    await ctx.reply('*Manbalar*:\n\n' + sourceReplyMessage, {
+        parse_mode: 'Markdown',
+        disable_web_page_preview: true
+    });
+    ctx.scene.exit()
+});
+
+export default scene;
