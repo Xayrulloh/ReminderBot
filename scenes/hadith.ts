9a49afab35555a338d70dc5f422044385ae6a1ad
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2023-11-18 20:44:55 +0500

    feat: forward mode and link to bot added

--- scenes/hadith.ts
+++ scenes/hadith.ts
@@ -2,15 +2,21 @@ import { Scene } from 'grammy-scenes'
 import Model from '#config/database'
 import { BotContext } from '#types/context'
 import { IHadith } from '#types/database'
+import HLanguage from '#helper/language'
+import { InlineKeyboard } from 'grammy'
 
 const scene = new Scene<BotContext>('Hadith')
 
 scene.step(async (ctx) => {
+  const keyboard = new InlineKeyboard()
+  const enterMessage = HLanguage('enter')
   let hadith: IHadith[] | string = await Model.Hadith.aggregate<IHadith>([{ $sample: { size: 1 } }])
 
   hadith = '\n\n<pre>' + (hadith[0]?.content || 'Hozircha hadis mavjud emas') + '</pre>'
 
-  await ctx.reply(hadith, { parse_mode: 'HTML' })
+  keyboard.url(enterMessage, 'https://t.me/' + ctx.me.username)
+
+  await ctx.reply(hadith, { parse_mode: 'HTML', reply_markup: keyboard })
 
   ctx.scene.exit()
 })
