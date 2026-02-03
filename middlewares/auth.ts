0277c3ebe76ae9d9b8f8444f4bd437291ff15de4
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2026-02-03 14:48:49 +0500

    hotfix: nonAdminPermission only for our bot

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -74,6 +74,13 @@ export async function groupAuthMiddleware(ctx: BotContext, next: NextFunction) {
   ctx.group = group
 
   // 4. Check for admin
+  const isInteractingWithBot =
+    ctx.message?.entities?.some((entity) => entity.type === 'bot_command') ||
+    ctx.message?.reply_to_message?.from?.id === ctx.me?.id ||
+    (ctx.me?.username && ctx.message?.text?.includes(`@${ctx.me.username}`))
+
+  if (!isInteractingWithBot) return next()
+
   if (ctx.from && !ctx.myChatMember) {
     try {
       const admins = await ctx.getChatAdministrators()
